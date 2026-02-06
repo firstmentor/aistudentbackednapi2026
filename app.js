3233e2211e5a18803a90e2175269f3c96ce81ca9
firstmentore <jainvikas887@gmail.com>
2026-02-06 12:22:51 +0530

    dhgfg

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://aistudentbackednapi2026.onrender.com"
+    "https://pnaistudent.netlify.app"
   ],
   credentials: true
 }));
